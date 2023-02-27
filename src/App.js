5b998963f883e7780a00a4c9e2f5a368a18c3b80
priyaa74 <priyavarshney070497@gmail.com>
2023-02-27 23:20:51 +0530

    Timer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import ProgressBar from './components/ProgressBar';
 import './App.css';
 import Stopwatch from './components/Stopwatch';
+import Timer from './components/Timer';
 
 function App() {
+  const THREE_DAYS_IN_MS = 3 * 24 * 60 * 60 * 1000;
+  const NOW_IN_MS = new Date().getTime();
+
+  const dateTimeAfterThreeDays = NOW_IN_MS + THREE_DAYS_IN_MS;
   return (
     <div className="App">
       <ProgressBar/>
       <Stopwatch/>
+      <Timer targetDate={dateTimeAfterThreeDays} />
     </div>
   );
 }
---
+++ src/components/Timer.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import DateTimeDisplay from './DateTimeDisplay';
+import { Countdown } from './Countdown';
+
+const ExpiredNotice = () => {
+  return (
+    <div className="expired-notice">
+      <span>Expired!!!</span>
+      <p>Please select a future date and time.</p>
+    </div>
+  );
+};
+
+const ShowCounter = ({ days, hours, minutes, seconds }) => {
+  return (
+    <div className="show-counter">
+      
+        <DateTimeDisplay value={days} type={'Days'} isDanger={days <= 3} />
+        <p>:</p>
+        <DateTimeDisplay value={hours} type={'Hours'} isDanger={false} />
+        <p>:</p>
+        <DateTimeDisplay value={minutes} type={'Mins'} isDanger={false} />
+        <p>:</p>
+        <DateTimeDisplay value={seconds} type={'Seconds'} isDanger={false} />
+      
+    </div>
+  );
+};
+
+const Timer = ({ targetDate }) => {
+  const [days, hours, minutes, seconds] = Countdown(targetDate);
+
+  if (days + hours + minutes + seconds <= 0) {
+    return <ExpiredNotice />;
+  } else {
+    return (
+      <ShowCounter
+        days={days}
+        hours={hours}
+        minutes={minutes}
+        seconds={seconds}
+      />
+    );
+  }
+};
+
+export default Timer;
